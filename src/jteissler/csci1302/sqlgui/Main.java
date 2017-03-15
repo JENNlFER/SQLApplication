ab01c125208a5ec0352b115f3f9835b935d8e111
Graphica <jteissler@uga.edu>
2017-03-15 18:06:18 -0400

    Style UI

--- src/jteissler/csci1302/sqlgui/Main.java
+++ src/jteissler/csci1302/sqlgui/Main.java
@@ -1,9 +1,14 @@
 package jteissler.csci1302.sqlgui;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,10 +17,36 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Workbench.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 800, 600);
+        String css = getClass().getResource("Workbench.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
-        StringDatabase
+        ListView<String> list = (ListView) scene.lookup("#status-output");
+        ObservableList<String> items =  FXCollections.observableArrayList (
+                "Status 1", "Status 2", "Status 3", "Status 4", "Status 5");
+        list.setItems(items);
+        list.scrollTo(items.size() - 1);
+
+        ListView<String> list2 = (ListView) scene.lookup("#error-output");
+        ObservableList<String> items2 =  FXCollections.observableArrayList (
+                "Error 1", "Error 2", "Error 3", "Error 4", "Error 5");
+        list2.setItems(items2);
+        list2.scrollTo(items2.size() - 1);
+
+        TreeView<String> view = (TreeView) scene.lookup("#directory-tree");
+        TreeItem<String> rootnode = new TreeItem<>("SQL Workbench");
+        rootnode.setExpanded(true);
+
+        TreeItem item = new TreeItem<>("Database 2");
+        item.getChildren().add(new TreeItem<>("Table 1"));
+        rootnode.getChildren().addAll(
+                new TreeItem<>("Database 1"),
+                item ,
+                new TreeItem<>("Database 3")
+        );
+        view.setRoot(rootnode);
     }
 
 
