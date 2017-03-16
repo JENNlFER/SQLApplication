6cd4f8984981bdbc56b6efa083e4b4b39e16fcbb
ktenus <ket48060@uga.edu>
2017-03-16 13:39:15 -0400

    updating some more listeners such as master directory name and file extensions

--- src/jteissler/csci1302/sqlgui/SQLPreferences.java
+++ src/jteissler/csci1302/sqlgui/SQLPreferences.java
@@ -1,15 +1,22 @@
 package jteissler.csci1302.sqlgui;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 
 /**
  * Created by Kyle on 3/15/2017.
  */
 public class SQLPreferences {
+
+        boolean execHighlightedBoolean = false;
+        boolean printStatusHighlightedBoolean = false;
+        boolean printErrorHighlightedBoolean = false;
 
         @FXML
         private CheckBox execHighlighted;
@@ -46,22 +53,41 @@ public class SQLPreferences {
 
 
         @FXML
-        private void onExecHighlightedToggle(ActionEvent event){}
+        private void onExecHighlightedToggle(ActionEvent event){
+             if(execHighlightedBoolean) execHighlightedBoolean = false;
+             else execHighlightedBoolean = true;
+        }
 
         @FXML
-        private void onPrintStatusToggle(ActionEvent event){}
+        private void onPrintStatusToggle(ActionEvent event){
+                if(printStatusHighlightedBoolean) printStatusHighlightedBoolean = false;
+                else printStatusHighlightedBoolean = true;
+        }
 
         @FXML
-        private void onPrintErrorToggle(ActionEvent event){}
+        private void onPrintErrorToggle(ActionEvent event){
+                if(printErrorHighlightedBoolean) printErrorHighlightedBoolean = false;
+                else printErrorHighlightedBoolean = true;
+        }
 
         @FXML
-        private void onTableKeyTyped(ActionEvent event){}
-
+        private void onTableKeyTyped(KeyEvent event) {
+                if (tableFileExt.getText() != null) {
+                        WorkbenchOptions.TABLE_FILE_EXTENSION = tableFileExt.getText();
+                }
+        }
         @FXML
-        private void onSavedKeyTyped(ActionEvent event){}
-
+        private void onSavedKeyTyped(ActionEvent event) {
+                if (savedFileExt.getText() != null) {
+                        WorkbenchOptions.SAVE_FILE_EXTENSION = savedFileExt.getText();
+                }
+        }
         @FXML
-        private void onMasterDirectoryKeyTyped(ActionEvent event){}
+        private void onMasterDirectoryKeyTyped(ActionEvent event){
+                if(masterDirectoryName.getText() != null) {
+                        WorkbenchOptions.MASTER_DIRECTORY = masterDirectoryName.getText();
+                }
+        }
 
         @FXML
         private void onSemiToggle(ActionEvent event){}
