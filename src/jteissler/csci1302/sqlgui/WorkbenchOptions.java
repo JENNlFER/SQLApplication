672453b9ae08f0f0fb320a573a8adeb5b44c3804
Graphica <jteissler@uga.edu>
2017-03-15 21:26:48 -0400

    Add options

--- src/jteissler/csci1302/sqlgui/WorkbenchOptions.java
+++ src/jteissler/csci1302/sqlgui/WorkbenchOptions.java
@@ -6,5 +6,21 @@ package jteissler.csci1302.sqlgui;
  */
 public class WorkbenchOptions
 {
+	public static boolean EXECUTE_CURRENT_LINE = true;
+	public static boolean EXECUTE_HIGHLIGHTED = true;
+
+	public static boolean USE_STATUS_LOG = false;
+	public static boolean USE_ERROR_LOG = false;
+
+	public static String MASTER_DIRECTORY = "sql_data";
+	public static String SAVE_FILE_EXTENSION = "sql";
+	public static String TABLE_FILE_EXTENSION = "tab";
+
+	public static CommandDivider COMMAND_DIVIDER = CommandDivider.SEMICOLON;
+
+	public enum CommandDivider
+	{
+		EACH_LINE, EMPTY_LINE, SEMICOLON
+	}
 
 }
