263bfa23ec476ff3f69b73a5487f739fd4c07f36
ktenus <ket48060@uga.edu>
2017-03-16 14:12:22 -0400

    extensions and events updated

--- src/jteissler/csci1302/sqlgui/SQLPreferences.java
+++ src/jteissler/csci1302/sqlgui/SQLPreferences.java
@@ -40,62 +40,86 @@ public class SQLPreferences {
         private RadioButton semiButton;
 
         @FXML
-        private RadioButton endLineButton;
+        private RadioButton emptyLineButton;
 
         @FXML
-        private RadioButton colonButton;
+        private RadioButton eachLineButton;
 
         @FXML
-        private void initialize()
-        {
+        private void initialize() {
 
         }
 
 
         @FXML
-        private void onExecHighlightedToggle(ActionEvent event){
-             if(execHighlightedBoolean) execHighlightedBoolean = false;
-             else execHighlightedBoolean = true;
+        private void onExecHighlightedToggle(ActionEvent event) {
+                if (execHighlightedBoolean) execHighlightedBoolean = false;
+                else execHighlightedBoolean = true;
         }
 
         @FXML
-        private void onPrintStatusToggle(ActionEvent event){
-                if(printStatusHighlightedBoolean) printStatusHighlightedBoolean = false;
+        private void onPrintStatusToggle(ActionEvent event) {
+                if (printStatusHighlightedBoolean) printStatusHighlightedBoolean = false;
                 else printStatusHighlightedBoolean = true;
         }
 
         @FXML
-        private void onPrintErrorToggle(ActionEvent event){
-                if(printErrorHighlightedBoolean) printErrorHighlightedBoolean = false;
+        private void onPrintErrorToggle(ActionEvent event) {
+                if (printErrorHighlightedBoolean) printErrorHighlightedBoolean = false;
                 else printErrorHighlightedBoolean = true;
         }
 
         @FXML
         private void onTableKeyTyped(KeyEvent event) {
-                if (tableFileExt.getText() != null) {
+                if (tableFileExt.getText() != null && !(tableFileExt.getText().isEmpty())) {
                         WorkbenchOptions.TABLE_FILE_EXTENSION = tableFileExt.getText();
                 }
+                else
+                        WorkbenchOptions.TABLE_FILE_EXTENSION = "tab";
+
         }
+
         @FXML
-        private void onSavedKeyTyped(ActionEvent event) {
-                if (savedFileExt.getText() != null) {
+        private void onSavedKeyTyped(KeyEvent event) {
+                if (savedFileExt.getText() != null && !(tableFileExt.getText().isEmpty())) {
                         WorkbenchOptions.SAVE_FILE_EXTENSION = savedFileExt.getText();
                 }
+                else
+                        WorkbenchOptions.SAVE_FILE_EXTENSION = "sql";
         }
+
         @FXML
-        private void onMasterDirectoryKeyTyped(ActionEvent event){
-                if(masterDirectoryName.getText() != null) {
+        private void onMasterDirectoryKeyTyped(KeyEvent event) {
+                if (masterDirectoryName.getText() != null && !(tableFileExt.getText().isEmpty())) {
                         WorkbenchOptions.MASTER_DIRECTORY = masterDirectoryName.getText();
                 }
+                else
+                        WorkbenchOptions.MASTER_DIRECTORY = "sql_data";
         }
 
         @FXML
-        private void onSemiToggle(ActionEvent event){}
+        private void onSemiToggle(ActionEvent event) {
+                if(emptyLineButton.isSelected()) emptyLineButton.setSelected(false);
+                if(eachLineButton.isSelected()) eachLineButton.setSelected(false);
 
-        @FXML
-        private void onEndLineToggle(ActionEvent event){}
+                WorkbenchOptions.COMMAND_DIVIDER = WorkbenchOptions.CommandDivider.SEMICOLON;
 
+        }
         @FXML
-        private void onColonToggle(ActionEvent event){}
+        private void onEmptyLineToggle(ActionEvent event){
+                if(semiButton.isSelected()) semiButton.setSelected(false);
+                if(eachLineButton.isSelected()) eachLineButton.setSelected(false);
+
+                WorkbenchOptions.COMMAND_DIVIDER = WorkbenchOptions.CommandDivider.EMPTY_LINE;
+        }
+        @FXML
+        private void onEachLineToggle(ActionEvent event){
+                if(emptyLineButton.isSelected()) emptyLineButton.setSelected(false);
+                if(semiButton.isSelected()) semiButton.setSelected(false);
+
+                WorkbenchOptions.COMMAND_DIVIDER = WorkbenchOptions.CommandDivider.EACH_LINE;
+
+        }
+
 
 }
